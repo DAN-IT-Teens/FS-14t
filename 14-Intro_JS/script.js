017650ad2f4a99f2549b8a68565cee0ce81d6347
sswiatoslaw <sviatoslav.sodel@creditkasa.ua>
2024-02-01 12:46:26 +0200

    added fs-14t all files

---
+++ 14-Intro_JS/script.js
@@ -0,0 +1,17 @@
+let text;
+text = "Hello world!";
+const isString = typeof text;
+console.log(name);
+console.log(isString);
+
+const x = 10;
+const y = 5;
+
+console.log(x);
+console.log(y);
+
+let xy = x + y;
+
+console.log(xy);
+
+alert(name);
---
+++ 15-Strings/script.js
@@ -0,0 +1,32 @@
+const num1 = 2;
+const num2 = 5;
+
+const text = `Відповідь: ${num1 + num2}`;
+
+console.log(text);
+
+// const username = prompt("Введіть ваше імʼя");
+
+// if (username) {
+//     alert(`Вітаємо, ${username}`);
+// }
+
+const result1 = 1 + 2;
+console.log(result1);
+const result2 = "10" / "2";
+console.log(result2)
+const result3 = "2" + "5";
+console.log(result3);
+const result4 = 0; // to Boolean = false
+const result5 = 1; // to Boolean = true
+const result6 = null; // to number = 0;
+const result7 = undefined; // to number NaN
+
+// == звичайне порівняння
+// === строге порівняння
+// != не дорівнює
+
+
+console.log(0 == false);
+
+console.log(false !== 1);
