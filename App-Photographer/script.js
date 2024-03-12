f22c384459906af9b192b952443ffdeab3f62113
sswiatoslaw <sviatoslav.sodel@creditkasa.ua>
2024-03-12 15:31:34 +0200

    added photographer app

---
+++ App-Photographer/script.js
@@ -0,0 +1,30 @@
+const recentPosts = [
+    {
+        image: "./images/post-1.png",
+        title: "Aenean Adipiscing Etiam Vestibulum",
+        category: "Photography, Journal",
+        commentsAmount: 9,
+        description: "Etiam porta sem malesuada euismod. Aenean leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum."
+    },
+    {
+        image: "./images/post-2.png",
+        title: "Aenean Adipiscing Etiam Vestibulum",
+        category: "Photography, Journal",
+        commentsAmount: 9,
+        description: "Etiam porta sem malesuada euismod. Aenean leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum."
+    },
+    {
+        image: "./images/post-3.png",
+        title: "Aenean Adipiscing Etiam Vestibulum",
+        category: "Photography, Journal",
+        commentsAmount: 9,
+        description: "Etiam porta sem malesuada euismod. Aenean leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum."
+    },
+    {
+        image: "./images/post-4.png",
+        title: "Aenean Adipiscing Etiam Vestibulum",
+        category: "Photography, Journal",
+        commentsAmount: 9,
+        description: "Etiam porta sem malesuada euismod. Aenean leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum."
+    },
+]
