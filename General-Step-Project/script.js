e35385d7437e61451d41c7942f1253db86f07acb
Святослав Содель <sswiatoslaw@MacBook-Air-Svatoslav.local>
2024-03-26 18:58:57 +0200

    add step project 2 lesson

---
+++ General-Step-Project/script.js
@@ -0,0 +1,11 @@
+const cartButton = document.querySelector('.action-cart');
+
+const productList = [
+    {
+        id: 0,
+        title: "Vanilla Latte",
+        info: ['hot', 'cold'],
+        price: 21000,
+        img: "./images/card-1.png"
+    }
+]
