2dd8c4164f79f532df11f11d34f6888db2eaa4ef
Святослав Содель <sswiatoslaw@MacBook-Air-Svatoslav.local>
2024-05-02 19:03:41 +0300

    add lesson first mobile

--- Gulp-Project-Practice/app/scripts/menu.js
+++ Gulp-Project-Practice/app/scripts/menu.js
@@ -1,8 +1,6 @@
-
 const menuButton = document.querySelector(".header__button-menu");
 const menu = document.querySelector('.header .menu');
 
 menuButton.addEventListener("click", () => {
     menu.classList.toggle('menu--mobile');
 })
-console.log('Hello world!');
--- Gulp-Project-Practice/gulp/config/path.js
+++ Gulp-Project-Practice/gulp/config/path.js
@@ -1,7 +1,7 @@
 const path = {
     html: ['./app/index.html'],
     styles: ['./app/styles/*.scss', './app/style.scss'],
-    scripts: ['./app/scripts/index.js', './app/script.js'],
+    scripts: ['./app/scripts/*.js', './app/script.js'],
     images: ['./app/images/*.svg', './app/images/*.png']
 }
 
